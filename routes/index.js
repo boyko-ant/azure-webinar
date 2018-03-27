5985e88c648f307e4f179c047c1656f0c37087e7
Anton Boyko <me@boykoant.pro>
2018-03-28 00:55:57 +0300

    config

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var nconf = require("nconf");
+
+nconf
+  .env()
+  .defaults({
+    name: "Azure Webinar"
+  });
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: nconf.get("name") });
 });
 
 module.exports = router;
