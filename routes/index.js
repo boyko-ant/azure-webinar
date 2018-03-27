575105afc1a6c9016d82fc97e62b710b00f958d6
Anton Boyko <me@boykoant.pro>
2018-03-28 00:09:09 +0300

    Revert "config"
    This reverts commit 3ce91549006be9bae5be0c64d7ecf2a82e02e9dd.

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var nconf = require("nconf");
-
-nconf
-  .env()
-  .defaults({
-    name: "Azure Webinar"
-  });
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: nconf.get("name") });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
